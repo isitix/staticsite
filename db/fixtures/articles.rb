b2420b3a5d1fff3449d03837b934d507cef8d501
mikael.dautrey <mikael.dautrey@isitix.com>
2015-12-12 07:28:21 +0100

    version ok avant simplification du code

--- db/fixtures/articles.rb
+++ db/fixtures/articles.rb
@@ -90,8 +90,8 @@ end
 Article.seed do |s|
   s.id = 6
   s.title = "FAQ"
-  s.small_picture = "faq_small.jpg"
-  s.first_picture = "faq_first.jpg"
+  s.small_picture = "question_small.jpg"
+  s.first_picture = "question_first.jpg"
   s.small_content = "Les questions que nos clients nous posent le plus fréquemment..."
 
   s.big_content = "
