f79c6eb45409fd4d6db117af29bc4066308b83ba
Mikael DAUTREY <mikael.dautrey@isitix.com>
2018-04-12 16:10:36 +0200

    version finale

--- db/migrate/20180409162306_add_language_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddLanguageToArticles < ActiveRecord::Migration
-  def change
-    add_column :articles, :language, :string, :default => 'FR'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409162306) do
+ActiveRecord::Schema.define(version: 20151216131748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title", limit: 255
-    t.string "small_picture", limit: 255
-    t.string "keywords", limit: 255
+    t.string   "title",         limit: 255
+    t.string   "small_picture", limit: 255
+    t.string   "keywords",      limit: 255
     t.text     "small_content"
     t.text     "big_content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_picture", limit: 255
-    t.integer "article_id", default: 0
-    t.integer "priority", default: 1
-    t.boolean "visibility", default: true
-    t.string "icon", limit: 255
-    t.integer "article_id_id"
-    t.string "language", default: "FR"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "first_picture", limit: 255
+    t.integer  "article_id",                default: 0
+    t.integer  "priority",                  default: 1
+    t.boolean  "visibility",                default: true
+    t.string   "icon",          limit: 255
+    t.integer  "article_id_id"
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "company", limit: 255
-    t.string "phone", limit: 255
-    t.string "email", limit: 255
-    t.string "content", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.string   "company",    limit: 255
+    t.string   "phone",      limit: 255
+    t.string   "email",      limit: 255
+    t.string   "content",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
